e893bc2df8281235cfa9b46f48571ebf45be03f6
Lokesh-1612 <lokeshnayak1612@gmail.com>
2026-04-02 16:03:30 +0530

    Modified Addition.java

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ class Addition {
         int a = 5;
         int b = 10;
         int c = a + b;
-        System.out.println("The sum is: " + c);
+        System.out.println("The sum is:  " + c);
     }
 }
